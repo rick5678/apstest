dbc82e68fa4b0da2aac1ebbbf76b01e052e8571a
rick5678 <henriquefrancoaraujo@gmail.com>
2021-11-16 21:48:10 -0300

    first commit

---
+++ infoEmpresa.py
@@ -0,0 +1,14 @@
+class infoEmpresa:
+    def __init__(self, produtora, endereco, prodAnual, numFunc, 
+    destProd, qntMaq, nvlAuto):
+        self.produtora = produtora
+        self.endereco = endereco
+        self.prodAnual = prodAnual
+        self.numFunc = numFunc
+        self.destProd = destProd
+        self.qntMaq = qntMaq
+        self.nvlAuto = nvlAuto
+
+empresa1 = infoEmpresa('CENTRAL AÇUCAREIRA SANTO ANTONIO S.A.', 'São Luís do Quitunde, ZONA RURAL, AL', '750 mil', '380', 'Exportação', 'Não informado', 'Não informado')
+
+empresa2 = infoEmpresa('CENTRAL AÇUCAREIRA SANTO ANTONIO S.A.', 'São Luís do Quitunde, ZONA RURAL, AL', '750 mil', '380', 'Exportação', 'Não informado', 'Não informado')
---
+++ jogoteca.py
@@ -0,0 +1,96 @@
+from flask import Flask, render_template, request, redirect, session, flash, url_for
+import cv2
+import numpy as np
+from infoEmpresa import *
+
+app = Flask(__name__)
+app.secret_key = 'alura'
+
+
+class Produto:
+    def __init__(self, nome):
+        self.nome = nome
+
+
+class Usuario:
+    def __init__(self, id, nome, senha, cargo):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
+        self.cargo = cargo
+
+
+usuario1 = Usuario('gustavo', 'Gustavo', cv2.imread('digital.jpg'), '2')
+usuario2 = Usuario('arion', 'Arion', '1234', '2')
+usuario3 = Usuario('henrique', 'Henrique', '1234', 'CLI')
+
+usuarios = {usuario1.id: usuario1,
+            usuario2.id: usuario2,
+            usuario3.id: usuario3}
+
+empresas = [empresa1, empresa2]
+
+produto1 = Produto('Açucar')
+produto2 = Produto('alcool')
+produto3 = Produto('energia')
+lista = [produto1, produto2, produto3]
+
+
+@app.route('/')
+def index():
+    return render_template('login.html', titulo='Agrotoxicos', Produtos=lista)
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html', empresa=empresas)
+
+
+@app.route('/novo')
+def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
+    return render_template('novo.html', titulo='Novo Produto')
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima,)
+
+@app.route('/fiscal')
+def fiscal():
+    proxima = request.args.get('proxima')
+    return render_template('fiscal.html',)
+
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        imgLogada = cv2.imread(request.form['senha'])
+        dif = cv2.subtract(imgLogada, usuario.senha)
+        result = not np.any(dif)
+        if result is True:
+            session['usuario_logado'] = usuario.id
+            flash(usuario.nome + ' logou com sucesso!')
+            proxima_pagina = request.form['proxima']
+            if usuario.cargo == '2':
+                return render_template('home.html', titulo=usuario.nome,  usuario=usuario, empresa=empresas)
+            if usuario.cargo == '1':
+                return render_template('home.html', titulo=usuario.nome,  Produtos=lista, usuario=usuario, empresa=empresas)
+        return redirect(proxima_pagina)
+    else:
+        flash('Não logado, tente novamente!')
+        return redirect(url_for('login'))
+    
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum usuário logado!')
+
+    return redirect(url_for('index'))
+
+
+app.run(debug=True)
